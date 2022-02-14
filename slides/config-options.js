de744f7403a2157f96755377bbd0052b9ed45fb8
DOSull <david.osullivan@vuw.ac.nz>
2022-02-14 15:42:32 +1300

    updated slide to reveal.js current for Feb 2022

--- slides/config-options.js
+++ slides/config-options.js
@@ -3,46 +3,13 @@
 Reveal.initialize({
   controls: true,
   progress: true,
-  loop: false,
-  slideNumber: true,
-  history: true,
+  slideNumber: "c/t",
   center: true,
-  rollingLinks: false,
-  previewLinks: false,
-  viewDistance: 3,
-  touch: true,
+  hash: true,
   width: 1600,
   height: 1200,
-  margin: 0.025,
-  minScale: 0.2,
-  maxScale: 1.0,
-  theme: Reveal.getQueryHash().theme, // available themes are in /css/theme
-  backgroundTransition: 'slide',
-  transition: 'linear', //Reveal.getQueryHash().transition || 'default', // default/cube/page/concave/zoom/linear/fade/none
-  // math: {
-  //   mathjax: 'http://cdn.mathjax.org/mathjax/latest/MathJax.js',
-  //   config: 'TeX-AMS_HTML-full'  // See http://docs.mathjax.org/en/latest/config-files.html
-  // },
-  dependencies: [
-    { src: '../reveal.js/lib/js/classList.js', condition: function() { return !document.body.classList; } },
-    { src: '../reveal.js/plugin/markdown/marked.js', condition: function() { return !!document.querySelector( '[data-markdown]' ); } },
-    { src: '../reveal.js/plugin/markdown/markdown.js', condition: function() { return !!document.querySelector( '[data-markdown]' ); } },
-    { src: '../reveal.js/plugin/highlight/highlight.js', async: true, callback: function() { hljs.initHighlightingOnLoad(); } },
-    // { src: '../reveal.js/plugin/zoom-js/zoom.js', async: true, condition: function() { return !!document.body.classList; } },
-    // { src: '../reveal.js/plugin/notes/notes.js', async: true, condition: function() { return !!document.body.classList; } },
-    // { src: '../reveal.js/plugin/leap/leap.js', async: true },
-    // { src: '../reveal.js/plugin/search/search.js', async: true, condition: function() { return !!document.body.classList; } },
-    // { src: '../reveal.js/plugin/remotes/remotes.js', async: true, condition: function() { return !!document.body.classList; } },
-    { src: '../reveal.js/plugin/math/math.js', async: true }
-  ]
+  history: true,
+
+  // Learn about plugins: https://revealjs.com/plugins/
+  plugins: [ RevealZoom, RevealNotes, RevealSearch, RevealMarkdown, RevealHighlight ]
 });
-//
-// var header = $("#header").html;
-// if ( window.location.search.match( /print-pdf/gi ) ) {
-//     Reveal.addEventListener( 'ready', function( event ) {
-//      $("body").remove("#header");
-//     });
-// }
-// else {
-//   $('div.reveal').append(header);
-// }
